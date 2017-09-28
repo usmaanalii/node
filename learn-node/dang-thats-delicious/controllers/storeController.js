e4bf26eb206fdf3255505facd734bc01803e938e
usyyy <usmaanalii@hotmail.co.uk>
2017-09-28 19:42:31 +0100

    Completed
    Lesson 20 — Using Pre-Save hooks to make Unique Slugs
    Lesson 21 — Custom MongoDB Aggregations
    Lesson 22 — Multiple Query Promises with AsyncAwait

--- learn-node/dang-thats-delicious/controllers/storeController.js
+++ learn-node/dang-thats-delicious/controllers/storeController.js
@@ -95,3 +95,18 @@ exports.getStoreBySlug = async (req, res, next) => {
     store
   });
 }
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  
+  res.render('tag', {
+    title: 'Tags',
+    tags,
+    stores,
+    tag
+  });
+}
--- learn-node/dang-thats-delicious/models/Store.js
+++ learn-node/dang-thats-delicious/models/Store.js
@@ -36,14 +36,27 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next();
     return;
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of wes, wes-1, wes-2 ...
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
-  // TODO: uniqueness
 });
+
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+  ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
